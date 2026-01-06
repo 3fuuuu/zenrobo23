38c006fbdd6c26203c8dd432f2a45be48bd6fcd9
3fuuuu <fuuuu1009@icloud.com>
2026-01-06 18:20:32 +0900

    commit

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
 
-// put function declarations here:
-int myFunction(int, int);
+const char ssid[] = "ESP32AP-TEST";
+const char pass[] = "12345678";       // パスワードは8文字以上
+const IPAddress ip(192,168,123,45);
+const IPAddress subnet(255,255,255,0);
+AsyncWebServer server(80);            // ポート設定
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+
+  // SPIFFSのセットアップ
+  if(!SPIFFS.begin(true)){
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  
+  WiFi.softAP(ssid, pass);           // SSIDとパスの設定
+  delay(100);                        // このdelayを入れないと失敗する場合がある
+  WiFi.softAPConfig(ip, ip, subnet); // IPアドレス、ゲートウェイ、サブネットマスクの設定
+  
+  IPAddress myIP = WiFi.softAPIP();  // WiFi.softAPIP()でWiFi起動
+
+  // 各種情報を表示
+  Serial.print("SSID: ");
+  Serial.println(ssid);
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+
+  // GETリクエストに対するハンドラーを登録
+  // rootにアクセスされた時のレスポンス
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html");
+  });
+  // style.cssにアクセスされた時のレスポンス
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // サーバースタート
+  server.begin();
+
+  Serial.println("Server start!");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
