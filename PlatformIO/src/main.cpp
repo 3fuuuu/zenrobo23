eab34f051d234811f1672a54d48f1397bc7016f6
3fuuuu <fuuuu1009@icloud.com>
2026-01-16 17:12:46 +0900

    update

--- PlatformIO/src/main.cpp
+++ PlatformIO/src/main.cpp
@@ -2,12 +2,54 @@
 #include <WiFi.h>
 #include "ESPAsyncWebSrv.h"
 #include "SPIFFS.h"
+#include <ArduinoJson.h>
 
 const char ssid[] = "ESP32AP-TEST";
 const char pass[] = "12345678";       // パスワードは8文字以上
 const IPAddress ip(192,168,123,45);
 const IPAddress subnet(255,255,255,0);
 AsyncWebServer server(80);            // ポート設定
+
+const int L_PWM = 0;
+const int L_DIR = 0;
+const int R_PWM = 0;
+const int R_DIR = 0;
+
+
+void executeCommands(JsonArrayConst commands) {
+  Serial.println("Sequence Received ");
+
+  int index = 0;
+  for (JsonObjectConst cmd : commands) {
+    const char* type = cmd["type"] | "";
+
+    Serial.print("#");
+    Serial.print(index++);
+    Serial.print(" TYPE=");
+    Serial.println(type);
+
+    if (strcmp(type, "MOVE") == 0) {
+      float x = cmd["x"] | 0;
+      float y = cmd["y"] | 0;
+
+      Serial.print("  Move to X=");
+      Serial.print(x);
+      Serial.print(" Y=");
+      Serial.println(y);
+    }
+    else if (strcmp(type, "WAIT") == 0) {
+      int ms = cmd["ms"] | 0;
+
+      Serial.print("  Wait ");
+      Serial.print(ms);
+      Serial.println(" ms");
+
+      delay(ms);
+    }
+  }
+
+  Serial.println("Sequence End");
+}
 
 void setup()
 {
@@ -41,6 +83,39 @@ void setup()
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
+  server.on(
+    "/command",
+    HTTP_POST,
+    [](AsyncWebServerRequest *request){
+      Serial.println(">>> HTTP POST /command");
+      request->send(200, "text/plain", "OK");
+    },
+    NULL,
+    [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t, size_t) {
+
+      Serial.println(">>> JSON RECEIVED");
+      Serial.print("Length: ");
+      Serial.println(len);
+
+      StaticJsonDocument<2048> doc;
+      DeserializationError err = deserializeJson(doc, data, len);
+
+      if (err) {
+        Serial.print("JSON parse failed: ");
+        Serial.println(err.c_str());
+        return;
+      }
+
+      Serial.println("--- RAW JSON ---");
+      serializeJsonPretty(doc, Serial);
+      Serial.println();
+      Serial.println("----------------");
+
+      JsonArray commands = doc["commands"];
+      executeCommands(commands);
+    }
+  );
+  
   // サーバースタート
   server.begin();
 
